7a0cf3689d572a61e3f404ef55e9dbcece7db235
Patakh <108473337+Patakh@users.noreply.github.com>
2023-01-31 20:15:40 +0300

    Create server and CRUD Operations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using BioNetWork.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.ComponentModel;
 using System.Diagnostics;
+using BioNetWork.Models.Users;
 
 namespace BioNetWork.Controllers
 {
@@ -12,21 +15,53 @@ namespace BioNetWork.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
+        public IActionResult MyPublications()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Friends()
         {
             return View();
+           
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public UsersModel users = new UsersModel();
+        public IActionResult Users()
+        {
+            String connectionString = "Data Source=DESKTOP-2EN7EE8\\SQLEXPRESS;Initial Catalog=users;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String sql = "SELECT * FROM personal_data";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                           
+
+                            users.id = "" + reader.GetInt32(0);
+                            users.name = reader.GetString(1);
+                            users.email = reader.GetString(2);
+                            users.phone = reader.GetString(3);
+                            users.address = reader.GetString(4);
+                            users.date_of_registration = reader.GetDateTime(5).ToString();
+                           
+                        }
+
+                    }
+                }
+                connection.Close();
+            }
+            return View("Users", users);
+        }
+
     }
 }
---
+++ Models/Users/UsersModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BioNetWork.Models.Users
+{
+    public class UsersModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string date_of_registration { get; set; }
+
+    }
+
+}
